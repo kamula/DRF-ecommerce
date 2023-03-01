2b2d90dac51397a833eaf239de2b265b3ade937e
kamula <kamulaisaac@gmail.com>
2023-03-01 11:41:58 +0300

    drf installation

--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -29,6 +29,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # external packages
+    'rest_framework',
+
+    # internal apps
 ]
 
 MIDDLEWARE = [
@@ -108,3 +112,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {}
