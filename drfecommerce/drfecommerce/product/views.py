3987bea650ff16a67ac7b4780d3ce99d3b1d06ae
kamula <kamulaisaac@gmail.com>
2023-03-03 11:55:10 +0300

    schema declaration

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
 from .models import Category
 from .serializers import CategorySerializer
 
-
+@extend_schema(responses=CategorySerializer)
 class CategoryViewSet(viewsets.ViewSet):
     """
     A simple viewset for viewing all categories
     """
 
     queryset = Category.objects.all()
+
 
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
